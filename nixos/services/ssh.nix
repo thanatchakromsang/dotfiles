a7f68592a97c654512025d732a6c861c5421d91c
Thanatchaya Kromsaeng <thanatchakromsang@gmail.com>
2021-08-01 17:00:41 +0700

    fix(ssh.nix): disable password auth / ssh extraConfig

--- nixos/services/ssh.nix
+++ nixos/services/ssh.nix
@@ -7,8 +7,22 @@ in
   services.openssh = {
     enable = true;
     permitRootLogin = "yes";
-    passwordAuthentication = lib.mkDefault true;
+    passwordAuthentication = lib.mkDefault false;
   };
+
+  programs.ssh.extraConfig = "
+Host gitlab.com
+  HostName gitlab.com
+  Preferredauthentications publickey
+  AddKeysToAgent yes
+  IdentityFile ~/.ssh/personal
+
+Host github.com
+  Hostname github.com
+  Preferredauthentications publickey
+  AddKeysToAgent yes
+  IdentityFile ~/.ssh/personal
+  ";
 
   users.extraUsers.root.openssh.authorizedKeys.keys = lib.mkDefault [ pubkey.thanatchaya ];
 
