a5b1b015db12b6d064dffcbf4ea06b4bec81c4b0
Thanatchaya Kromsaeng <thanatchakromsang@gmail.com>
2021-08-03 08:51:30 +0700

    feat(git): add git gpg signing keys

--- nixos/users/thanatchaya/personal.nix
+++ nixos/users/thanatchaya/personal.nix
@@ -3,9 +3,13 @@
 {
   home-manager.users.thanatchaya = { pkgs, ... }: {
     programs.git = {
-        enable    = true;
-        userName  = "Thanatchaya Kromsaeng";
-        userEmail = "thanatchakromsang@gmail.com";
+      enable = true;
+      userName = "Thanatchaya Kromsaeng";
+      userEmail = "thanatchakromsang@gmail.com";
+      signing = {
+        signByDefault = true;
+        key = "5254D8A1FBCE25D519C7E3D8E405CB1740495F06";
+      };
     };
   };
 }
