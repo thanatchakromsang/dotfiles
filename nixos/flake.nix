84440eb8427c4a540e264d6c475c8b08ea07e4da
Thanatchaya Kromsaeng <thanatchakromsang@gmail.com>
2021-08-04 00:50:51 +0700

    fix(nix-flake): disable sops-nix

--- nixos/flake.nix
+++ nixos/flake.nix
@@ -4,7 +4,8 @@
     nixpkgs-unstable.url = "github:nixos/nixpkgs/nixos-unstable";
     nur.url = "github:nix-community/NUR";
     nixos-hardware.url = "github:NixOS/nixos-hardware";
-    sops-nix.url = "github:Mic92/sops-nix";
+    # # TODO: use sops-nix for secrets
+    # sops-nix.url = "github:Mic92/sops-nix";
     home-manager = {
       url = "github:nix-community/home-manager/release-21.05";
       inputs.nixpkgs.follows = "nixpkgs";
@@ -18,7 +19,7 @@
     , home-manager
     , nur
     , nixos-hardware
-    , sops-nix
+      # , sops-nix
     }@inputs:
       let
         inherit (nixpkgs) lib;
@@ -41,15 +42,17 @@
                 nixpkgs.overlays = [
                   nur.overlay
                   overlays.unstable
-                  # sops-nix.overlay
                 ];
                 nixpkgs.config.allowUnfree = true;
+
+                home-manager.useUserPackages = true;
+
                 system.configurationRevision = nixpkgs.lib.mkIf (self ? rev) self.rev;
               }
               nixos-hardware.nixosModules.lenovo-thinkpad-t14s-amd-gen1
               nixpkgs.nixosModules.notDetected
               home-manager.nixosModules.home-manager
-              sops-nix.nixosModules.sops
+              # sops-nix.nixosModules.sops
               ./machines/t14s/configuration.nix
             ];
             # extraArgs = { inputs = inputs; };
