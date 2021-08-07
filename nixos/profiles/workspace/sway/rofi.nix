09b07d3bfe12384a86c66a7d3dfd300731914183
Thanatchaya Kromsaeng <thanatchakromsang@gmail.com>
2021-08-07 12:51:29 +0700

    chore(flake.lock): update lock files

--- nixos/profiles/workspace/sway/rofi.nix
+++ nixos/profiles/workspace/sway/rofi.nix
@@ -4,9 +4,42 @@
 let
   fonts = config.themes.fonts;
   colors = config.themes.colors;
+  # https://discourse.nixos.org/t/install-shell-script-on-nixos/6849/10
+  rofi-poweroff = pkgs.writeScriptBin "rofi-poweroff" ''
+    #!${pkgs.bash}/bin/bash
+    case $(${pkgs.rofi}/bin/rofi -dmenu << EOF | sed 's/^ *//'
+    shutdown
+    reboot
+    logoff
+    hibernate
+    sleep
+    lock
+    console
+    EOF
+    ) in
+        "shutdown")
+            ${pkgs.systemd}/bin/systemctl poweroff
+            ;;
+        "reboot")
+            ${pkgs.systemd}/bin/systemctl reboot
+            ;;
+        "hibernate")
+            ${pkgs.systemd}/bin/systemctl hibernate
+            ;;
+        "sleep")
+            ${pkgs.systemd}/bin/systemctl suspend
+            ;;
+        "lock")
+            ${pkgs.swaylock}/bin/swaylock -f -c 000000
+            ;;
+        "logoff")
+            ${pkgs.sway}/bin/swaymsg exit
+            ;;
+    esac
+  '';
 in
 {
-  home-manager.users.thanatchaya = { pkgs, ... }: {
+  home-manager.users.thanatchaya = {
     programs.rofi = {
       enable = true;
       width = 30;
@@ -16,7 +49,7 @@ in
       borderWidth = 3;
       rowHeight = 1;
       # FIXME(rofi): using terminal variable from config
-      terminal = "kitty";
+      terminal = "${pkgs.kitty}/bin/kitty";
       separator = "none";
       scrollbar = false;
       theme = "gruvbox-dark";
@@ -28,16 +61,19 @@ in
 
     home.packages = with pkgs; [
       networkmanager_dmenu
+      rofi-poweroff
+      bitwarden-rofi
+      rofi-bluetooth
     ];
 
     # FIXME(networkmanager-dmenu): using terminal variables from config
     xdg.configFile."networkmanager-dmenu/config.ini".text = ''
       [dmenu]
-      dmenu_command = rofi
+      dmenu_command = ${pkgs.rofi}/bin/rofi
       rofi_highlight = True
 
       [editor]
-      terminal = kitty
+      terminal = ${pkgs.kitty}/bin/kitty
       gui_if_available = true
     '';
   };
