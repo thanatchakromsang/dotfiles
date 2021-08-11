16a3bb1cc15e7f54a2f7f7fa59edeff1a799031d
Thanatchaya Kromsaeng <thanatchakromsang@gmail.com>
2021-08-12 01:38:05 +0700

    feat(nvim): gruvbox-material styling

--- configs/nvim/lua/colorscheme.lua
+++ configs/nvim/lua/colorscheme.lua
@@ -1,5 +1,7 @@
 vim.g.gruvbox_material_background = 'soft'
 vim.g.gruvbox_material_background = 'soft'
+vim.g.gruvbox_material_enable_italic = 0
+vim.g.gruvbox_material_disable_italic_comment = 1
 vim.g.gruvbox_material_better_performance = 1
 vim.o.termguicolors=true
 vim.cmd('colorscheme gruvbox-material')
