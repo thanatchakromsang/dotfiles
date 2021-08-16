a9631e16e9c09f4fca233eebf85f27189c8acc42
Thanatchaya Kromsaeng <thanatchakromsang@gmail.com>
2021-08-17 04:35:44 +0700

    fix(keyboards): update crkbd debounce rate

--- keyboards/symlinks.sh
+++ keyboards/symlinks.sh
@@ -1,11 +1,11 @@
-#!/bin/bash
+#!/usr/bin/env bash
 
-DIR="$( cd "$( dirname "${BASH_SOURCE[0]}" )" >/dev/null 2>&1 && pwd )"
+DIR="$(cd "$(dirname "${BASH_SOURCE[0]}")" >/dev/null 2>&1 && pwd)"
 
 echo "*** Symlinks QMK Profile... ***"
-ln -s $DIR/users/thanatchaya $DIR/qmk/users
+ln -s "$DIR"/users/thanatchaya "$DIR"/qmk/users
 
 echo "*** Symlinks Keymap ***"
-ln -s $DIR/ergodox-ez $DIR/qmk/keyboards/ergodox_ez/keymaps/thanatchaya
-ln -s $DIR/keyboardio/atreus $DIR/qmk/keyboards/keyboardio/atreus/keymaps/thanatchaya
-ln -s $DIR/crkbd $DIR/qmk/keyboards/crkbd/keymaps/thanatchaya
+ln -s "$DIR"/ergodox-ez "$DIR"/qmk/keyboards/ergodox_ez/keymaps/thanatchaya
+ln -s "$DIR"/keyboardio/atreus "$DIR"/qmk/keyboards/keyboardio/atreus/keymaps/thanatchaya
+ln -s "$DIR"/crkbd "$DIR"/qmk/keyboards/crkbd/keymaps/thanatchaya
