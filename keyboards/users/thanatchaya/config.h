e2ca588cd7b5e8c868abdef4997e594be7bd4a3f
Thanatchaya Kromsaeng <thanatchakromsang@gmail.com>
2021-09-05 23:52:17 +0700

    feat(keyboards): update qmk mouse config

--- keyboards/users/thanatchaya/config.h
+++ keyboards/users/thanatchaya/config.h
@@ -26,10 +26,12 @@
 #define PERMISSIVE_HOLD
 
 // Mouse setting
-#define MOUSEKEY_INTERVAL       20
+# undef MOUSEKEY_INTERVAL
+# undef MOUSEKEY_MAX_SPEED
+#define MOUSEKEY_INTERVAL       16
 #define MOUSEKEY_DELAY          0
 #define MOUSEKEY_TIME_TO_MAX    60
-#define MOUSEKEY_MAX_SPEED      7
+#define MOUSEKEY_MAX_SPEED      3
 #define MOUSEKEY_WHEEL_DELAY    0
 
 #define COMBO_COUNT 5
