75114a46dd1f6a245f7c545adb41e4d90b79b81b
Thanatchaya Kromsaeng <thanatchakromsang@gmail.com>
2021-09-16 10:17:08 +0700

    feat(keyboards): bring back symbol layer

--- keyboards/users/thanatchaya/thanatchaya_combos.c
+++ keyboards/users/thanatchaya/thanatchaya_combos.c
@@ -1,18 +1,18 @@
 #include "thanatchaya_combos.h"
 
-// Horizontal combo
-const uint16_t PROGMEM k_l_quot_combo[] = {LALT_T(KC_K), LCTL_T(KC_L), COMBO_END};
-const uint16_t PROGMEM s_d_mins_combo[] = {LCTL_T(KC_S), LALT_T(KC_D), COMBO_END};
-
 const uint16_t PROGMEM s_e_esc_combo[] = {LCTL_T(KC_S), KC_E, COMBO_END};
+const uint16_t PROGMEM e_s_esc_combo[] = {KC_E, LCTL_T(KC_S), COMBO_END};
 const uint16_t PROGMEM i_l_esc_combo[] = {KC_I, LCTL_T(KC_L), COMBO_END};
+const uint16_t PROGMEM l_i_esc_combo[] = {LCTL_T(KC_L), KC_I, COMBO_END};
 
   /* ---------------------------------------------------------------------
    *
+   *   Qwerty layout
+   *
    *     ┌─────┬─────┬─────┬─────┬─────┐   ┌─────┬─────┬─────┬─────┬─────┐
    *     │     │     │ C   │     │     │   │     │     │   E │     │     │
-   *     ├─────┼─────S─────┼─────┼─────┤   ├─────┼─────┼─────S─────┼─────┤
-   *     │     │   E -     │     │     │   │     │     │  QUOT C   │     │
+   *     ├─────┼─────S─────-─────┼─────┤   ├─────┼─────-─────S─────┼─────┤
+   *     │     │   E │     │     │     │   │     │     │     │ C   │     │
    *     ├─────┼─────┼─────┼─────┼─────┤   ├─────┼─────┼─────┼─────┼─────┤
    *     │     │     │     │     │     │   │     │     │     │     │     │
    *     └─────┴─────┴─────┴─────┴─────┘   └─────┴─────┴─────┴─────┴─────┘
@@ -20,7 +20,7 @@ const uint16_t PROGMEM i_l_esc_combo[] = {KC_I, LCTL_T(KC_L), COMBO_END};
 
 combo_t key_combos[COMBO_COUNT] = {
   COMBO(s_e_esc_combo, KC_ESC),
+  COMBO(e_s_esc_combo, KC_ESC),
   COMBO(i_l_esc_combo, KC_ESC),
-  COMBO(k_l_quot_combo, KC_QUOT),
-  COMBO(s_d_mins_combo, KC_MINS),
+  COMBO(l_i_esc_combo, KC_ESC)
 };
