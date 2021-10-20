40f459506720512c1628fda79087f8decda35444
Thanatchaya Kromsaeng <thanatchakromsang@gmail.com>
2021-10-20 09:56:32 +0700

    style(firefox): update firefox userChrome

--- nixos/profiles/applications/firefox.nix
+++ nixos/profiles/applications/firefox.nix
@@ -44,6 +44,53 @@ let
     "network.allow-experiments" = false;
   };
   userChrome = ''
+    /* Source file https://github.com/MrOtherGuy/firefox-csshacks/tree/master/chrome/autohide_main_toolbar.css made available under Mozilla Public License v. 2.0
+    See the above repository for updates as well as full license text. */
+
+    /* This style hides the main toolbar and shows it when the cursor is over the tabs toolbar as well as whenever the focus is inside nav-bar, such as when urlbar is focused. */
+
+    :root{ --uc-navbar-transform: -40px }
+    :root[uidensity="compact"]{ --uc-navbar-transform: -34px }
+
+    :root[sessionrestored] :where(#nav-bar,#PersonalToolbar,#tab-notification-deck){
+      transform: translateY(var(--uc-navbar-transform))
+    }
+
+    :root:is([customizing],[chromehidden*="toolbar"]) :where(#nav-bar,#PersonalToolbar,#tab-notification-deck){
+      transform: none !important;
+      opacity: 1 !important;
+    }
+
+    #nav-bar:not([customizing]){
+      opacity: 0;
+      transition:  transform 0s ease 0s, opacity 0s ease 0s !important;
+      position: relative;
+      z-index: 2;
+    }
+    #TabsToolbar{ position: relative; z-index: 3 }
+
+    /* Show when toolbox is focused, like when urlbar has received focus */
+    #navigator-toolbox:focus-within > .browser-toolbar{
+      transform: translateY(0);
+      opacity: 1;
+      transition-duration: 0s, 0s !important;
+      transition-delay: 0s !important;
+    }
+    /* Show when toolbox is hovered */
+    #titlebar:hover ~ .browser-toolbar,
+    #nav-bar:hover,
+    #nav-bar:hover + #PersonalToolbar{
+      transform: translateY(0);
+      opacity: 1;
+      transition-duration: 0s, 0s !important;
+      transition-delay: 0s !important;
+    }
+
+    /* Bookmarks toolbar needs so extra rules */
+    #PersonalToolbar{ transition: transform 400ms ease 1.8s !important; position: relative; z-index: 1 }
+
+    /* Move up the content view */
+    :root[sessionrestored]:not([inFullscreen]) > body > #browser{ margin-top: var(--uc-navbar-transform); }
   '';
 in
 {
@@ -55,7 +102,11 @@ in
 
     programs.firefox = {
       enable = true;
-      package = pkgs.firefox-wayland;
+      package = pkgs.firefox-wayland.override {
+        cfg = {
+          enableTridactylNative = true;
+        };
+      };
       profiles.personal = {
         id = 0;
         userChrome = userChrome;
@@ -71,12 +122,12 @@ in
         bitwarden
         darkreader
         ublock-origin
-        vimium
+        # vimium
         # tab-session-manager
         # tree-style-tab
         # i-dont-care-about-cookies
         # refined-github
-        # tridactyl # vimium alternative
+        tridactyl # vimium alternative
         leechblock-ng
       ];
     };
