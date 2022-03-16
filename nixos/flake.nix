ea26f4da0d9016a7070f1bb14c013b93c81539ee
Thanatchaya Kromsaeng <thanatchakromsang@gmail.com>
2022-03-16 12:11:43 +0700

    feat(nixos, msi): add msi conf

--- nixos/flake.nix
+++ nixos/flake.nix
@@ -1,8 +1,6 @@
 {
   inputs = {
     nixpkgs.url = "github:nixos/nixpkgs/nixos-unstable";
-    /* TODO: Revert back to unstable once build issues has been solved */
-    /* nixpkgs.url = "github:nixos/nixpkgs/nixos-unstable"; */
     /* nixpkgs-unstable.url = "github:nixos/nixpkgs/nixos-unstable"; */
     nur.url = "github:nix-community/NUR";
     nixos-hardware.url = "github:NixOS/nixos-hardware";
@@ -17,79 +15,99 @@
   outputs =
     { self
     , nixpkgs
-    # , nixpkgs-unstable
+      # , nixpkgs-unstable
     , home-manager
     , nur
     , nixos-hardware
       # , sops-nix
     }@inputs:
-      let
-        inherit (nixpkgs) lib;
-        # unstable-overlay = final: prev: {
-        #   unstable = (
-        #     import nixpkgs-unstable {
-        #       system = "x86_64-linux";
-        #       config.allowUnfree = true;
-        #     }
-        #   );
-        # };
-        overlays = [
-          # TODO: Refactor custom overlay
-          (
-            self: super: {
-              rofi-bluetooth = super.callPackage ./packages/rofi-bluetooth {};
-              # Override to create `kubectl-ctx`, `kubectl-ns` as kubectl plugins
-              kubectx = super.kubectx.overrideAttrs (
-                old: {
-                  postInstall = ''
-                    ${old.postInstall}
+    let
+      inherit (nixpkgs) lib;
+      # unstable-overlay = final: prev: {
+      #   unstable = (
+      #     import nixpkgs-unstable {
+      #       system = "x86_64-linux";
+      #       config.allowUnfree = true;
+      #     }
+      #   );
+      # };
+      overlays = [
+        # TODO: Refactor custom overlay
+        (
+          self: super: {
+            rofi-bluetooth = super.callPackage ./packages/rofi-bluetooth { };
+            # Override to create `kubectl-ctx`, `kubectl-ns` as kubectl plugins
+            kubectx = super.kubectx.overrideAttrs (
+              old: {
+                postInstall = ''
+                  ${old.postInstall}
 
-                    mkdir -p $out/bin
-                    cp kubectx $out/bin/kubectl-ctx
-                    cp kubens $out/bin/kubectl-ns
-                  '';
-                }
-              );
-            }
-          )
-          /* unstable-overlay */
-          nur.overlay
+                  mkdir -p $out/bin
+                  cp kubectx $out/bin/kubectl-ctx
+                  cp kubens $out/bin/kubectl-ns
+                '';
+              }
+            );
+          }
+        )
+        /* unstable-overlay */
+        nur.overlay
+      ];
+    in
+    {
+      nixosConfigurations.t14s = nixpkgs.lib.nixosSystem {
+        system = "x86_64-linux";
+        modules = [
+          {
+            nixpkgs.overlays = overlays;
+            nixpkgs.config.allowUnfree = true;
+            home-manager.useUserPackages = true;
+            system.configurationRevision = nixpkgs.lib.mkIf (self ? rev) self.rev;
+          }
+          nixos-hardware.nixosModules.lenovo-thinkpad-t14s-amd-gen1
+          nixpkgs.nixosModules.notDetected
+          home-manager.nixosModules.home-manager
+          # sops-nix.nixosModules.sops
+          ./modules
+          ./machines/t14s/configuration.nix
         ];
-      in
-        {
-          nixosConfigurations.t14s = nixpkgs.lib.nixosSystem {
-            system = "x86_64-linux";
-            modules = [
-              {
-                nixpkgs.overlays = overlays;
-                nixpkgs.config.allowUnfree = true;
-                home-manager.useUserPackages = true;
-                system.configurationRevision = nixpkgs.lib.mkIf (self ? rev) self.rev;
-              }
-              nixos-hardware.nixosModules.lenovo-thinkpad-t14s-amd-gen1
-              nixpkgs.nixosModules.notDetected
-              home-manager.nixosModules.home-manager
-              # sops-nix.nixosModules.sops
-              ./modules
-              ./machines/t14s/configuration.nix
-            ];
-            specialArgs = { inherit inputs; };
-          };
-          nixosConfigurations.canyon = nixpkgs.lib.nixosSystem {
-            system = "x86_64-linux";
-            modules = [
-              {
-                nixpkgs.overlays = overlays;
-                nixpkgs.config.allowUnfree = true;
-                home-manager.useUserPackages = true;
-                system.configurationRevision = nixpkgs.lib.mkIf (self ? rev) self.rev;
-              }
-              nixpkgs.nixosModules.notDetected
-              home-manager.nixosModules.home-manager
-              ./modules
-              ./machines/canyon/configuration.nix
-            ];
-            specialArgs = { inherit inputs; };
-          };
-        };
+        specialArgs = { inherit inputs; };
+      };
+      nixosConfigurations.canyon = nixpkgs.lib.nixosSystem {
+        system = "x86_64-linux";
+        modules = [
+          {
+            nixpkgs.overlays = overlays;
+            nixpkgs.config.allowUnfree = true;
+            home-manager.useUserPackages = true;
+            system.configurationRevision = nixpkgs.lib.mkIf (self ? rev) self.rev;
+          }
+          nixos-hardware.nixosModules.common-gpu-amd
+          nixpkgs.nixosModules.notDetected
+          home-manager.nixosModules.home-manager
+          ./modules
+          ./machines/canyon/configuration.nix
+        ];
+        specialArgs = { inherit inputs; };
+      };
+      nixosConfigurations.msi = nixpkgs.lib.nixosSystem {
+        system = "x86_64-linux";
+        modules = [
+          {
+            nixpkgs.overlays = overlays;
+            nixpkgs.config.allowUnfree = true;
+            home-manager.useUserPackages = true;
+            system.configurationRevision = nixpkgs.lib.mkIf (self ? rev) self.rev;
+          }
+          nixos-hardware.nixosModules.common-pc-laptop-ssd
+          nixos-hardware.nixosModules.common-cpu-intel
+          /* nixos-hardware.nixosModules.common-gpu-nvidia */
+          nixpkgs.nixosModules.notDetected
+          home-manager.nixosModules.home-manager
+          ./modules
+          ./machines/msi/configuration.nix
+        ];
+        specialArgs = { inherit inputs; };
+      };
+    };
 }
