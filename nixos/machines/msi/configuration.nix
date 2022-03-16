ce3da08398c1d2a43a8eff3fabafd43fc52a638d
Thanatchaya Kromsaeng <thanatchakromsang@gmail.com>
2022-03-16 09:46:11 +0700

    feat(nixos): add msi conf

---
+++ nixos/machines/msi/configuration.nix
@@ -0,0 +1,50 @@
+# Edit this configuration file to define what should be installed on
+# your system.  Help is available in the configuration.nix(5) man page
+# and in the NixOS manual (accessible by running ‘nixos-help’).
+
+{ config, pkgs, ... }:
+
+{
+  imports =
+    [
+      ./hardware-configuration.nix
+      ./override.nix
+
+      ../../profiles/workstation.nix
+      ../../profiles/desktop.nix
+      ../../profiles/notebook.nix
+      ../../profiles/development.nix
+      ../../profiles/gaming.nix
+
+      ../../profiles/monitoring/home-client.nix
+
+      ../../users/thanatchaya/personal.nix
+    ];
+
+  nix = {
+    package = pkgs.nixUnstable;
+    extraOptions = ''
+      experimental-features = nix-command flakes
+    '';
+  };
+
+  boot.loader = {
+    efi = {
+      canTouchEfiVariables = true;
+      efiSysMountPoint = "/boot";
+    };
+    grub = {
+      devices = [ "nodev" ];
+      efiSupport = true;
+    };
+  };
+
+  hardware.bluetooth.enable = true;
+
+  networking = {
+    hostName = "msi";
+    domain = "local";
+  };
+
+  system.stateVersion = "22.05";
+}
---
+++ nixos/machines/msi/override.nix
@@ -0,0 +1,50 @@
+{ config, pkgs, lib, ... }:
+
+let
+  monitor = "eDP-1";
+  clamshell-mode-reset = pkgs.writeScriptBin "clamshell-mode-reset" ''
+    #!${pkgs.bash}/bin/bash
+    if ${pkgs.gnugrep}/bin/grep -q open /proc/acpi/button/lid/LID*/state; then
+        ${pkgs.sway}/bin/swaymsg output ${monitor} enable
+    else
+        ${pkgs.sway}/bin/swaymsg output ${monitor} disable
+    fi
+  '';
+in
+{
+  home-manager.users.thanatchaya = {
+    wayland.windowManager.sway = {
+      config = {
+        workspaceOutputAssign = [
+          { workspace = "1"; output = "eDP-1"; }
+          { workspace = "2"; output = "eDP-1"; }
+          { workspace = "3"; output = "eDP-1"; }
+          { workspace = "4"; output = "eDP-1"; }
+          { workspace = "5"; output = "HDMI-A-1"; }
+          { workspace = "6"; output = "HDMI-A-1"; }
+          { workspace = "7"; output = "HDMI-A-1"; }
+          { workspace = "8"; output = "HDMI-A-1"; }
+          { workspace = "9"; output = "HDMI-A-1"; }
+          { workspace = "10"; output = "HDMI-A-1"; }
+        ];
+        output = {
+          "*".bg = "~/.dotfiles/wallpapers/gruvbox-dark-rainbow.png fill";
+          eDP-1 = {
+            resolution = "1920x1080";
+            position = "0 1080";
+          };
+          HDMI-A-1 = {
+            resolution = "1920x1080";
+            position = "0 0";
+          };
+        };
+      };
+      extraConfig = ''
+        bindswitch --reload --locked lid:on output ${monitor} disable
+        bindswitch --reload --locked lid:off output ${monitor} enable
+
+        exec_always ${clamshell-mode-reset}/bin/clamshell-mode-reset
+      '';
+    };
+  };
+}
