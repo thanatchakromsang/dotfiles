99c1c2b15378ebeee8b421508acc5eebf634c761
Thanatchaya Kromsaeng <thanatchakromsang@gmail.com>
2022-03-27 11:42:44 +0700

    feat(canyon): update override

--- nixos/machines/canyon/override.nix
+++ nixos/machines/canyon/override.nix
@@ -22,27 +22,27 @@
     wayland.windowManager.sway = {
       config = {
         workspaceOutputAssign = [
-          { workspace = "1"; output = "HDMI-A-2"; }
-          { workspace = "2"; output = "HDMI-A-2"; }
-          { workspace = "3"; output = "HDMI-A-2"; }
-          { workspace = "4"; output = "HDMI-A-2"; }
-          { workspace = "5"; output = "HDMI-A-1"; }
-          { workspace = "6"; output = "HDMI-A-1"; }
-          { workspace = "7"; output = "HDMI-A-1"; }
-          { workspace = "8"; output = "HDMI-A-1"; }
-          { workspace = "9"; output = "HDMI-A-1"; }
-          { workspace = "10"; output = "HDMI-A-1"; }
+          { workspace = "1"; output = "HDMI-A-1"; }
+          { workspace = "2"; output = "HDMI-A-1"; }
+          { workspace = "3"; output = "HDMI-A-1"; }
+          { workspace = "4"; output = "HDMI-A-1"; }
+          { workspace = "5"; output = "HDMI-A-2"; }
+          { workspace = "6"; output = "HDMI-A-2"; }
+          { workspace = "7"; output = "HDMI-A-2"; }
+          { workspace = "8"; output = "HDMI-A-2"; }
+          { workspace = "9"; output = "HDMI-A-2"; }
+          { workspace = "10"; output = "HDMI-A-2"; }
         ];
         output = {
           "*".bg = "~/.dotfiles/wallpapers/gruvbox-dark-rainbow.png fill";
           HDMI-A-1 = {
             resolution = "1920x1080";
             transform = "0";
-            position = "1920 0";
+            position = "0 0";
           };
           HDMI-A-2 = {
             resolution = "1920x1080";
-            position = "0 0";
+            position = "1920 0";
           };
         };
       };
