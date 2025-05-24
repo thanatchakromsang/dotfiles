ef7cf9e93601a7bbc13b4a88c7ce20819507edaf
Thanatchaya Kromsaeng <thanatchakromsang@gmail.com>
2025-05-24 04:19:09 +0000

    fmt

--- configs/nvim/lua/plugins/avante-ai.lua
+++ configs/nvim/lua/plugins/avante-ai.lua
@@ -1,72 +1,80 @@
-require('img-clip').setup ({
-  -- use recommended settings from above
+require("img-clip").setup({
+	-- use recommended settings from above
 })
-require('render-markdown').setup ({
-  -- use recommended settings from above
+
+require("render-markdown").setup({
+	-- use recommended settings from above
+  latex = {
+    enabled = false
+  }
 })
-require('avante_lib').load()
-require('avante').setup ({
-  provider = "claude",
-  claude = {
-    endpoint = "https://api.anthropic.com",
-    model = "claude-3-5-sonnet-20241022",
-    temperature = 0.0,
-    max_tokens = 2048,
-  },
-  dual_boost = {
-    enabled = false
-  },
-  behaviour = {
-    auto_suggestions = false, -- Experimental stage
-    auto_set_highlight_group = true,
-    auto_set_keymaps = true,
-    auto_apply_diff_after_generation = false,
-    support_paste_from_clipboard = false,
-    minimize_diff = true, -- Whether to remove unchanged lines when applying a code block
-  },
-  hints = {
-    enabled = false
-  },
-  windows = {
-    sidebar_header = {
-      rounded = false
-    },
-    edit = {
-      border = "none"
-    },
-    ask = {
-      border = "none"
-    }
-  },
-  mappings = {
-    diff = {
-      ours = "co",
-      theirs = "ct",
-      all_theirs = "ca",
-      both = "cb",
-      cursor = "cc",
-      next = "]x",
-      prev = "[x",
-    },
-    suggestion = {
-      accept = "<M-l>",
-      next = "<M-]>",
-      prev = "<M-[>",
-      dismiss = "<C-]>",
-    },
-    jump = {
-      next = "]]",
-      prev = "[[",
-    },
-    submit = {
-      normal = "<CR>",
-      insert = "<C-s>",
-    },
-    sidebar = {
-      apply_all = "A",
-      apply_cursor = "a",
-      switch_windows = "<Tab>",
-      reverse_switch_windows = "<S-Tab>",
-    },
-  },
+
+require("avante_lib").load()
+require("avante").setup({
+	provider = "claude",
+	claude = {
+		endpoint = "https://api.anthropic.com",
+		model = "claude-sonnet-4-20250514",
+		disable_tools = true,
+		temperature = 0.0,
+		max_tokens = 2048,
+	},
+	dual_boost = {
+		enabled = false,
+	},
+	behaviour = {
+		auto_suggestions = false, -- Experimental stage
+		auto_set_highlight_group = true,
+		auto_set_keymaps = true,
+    enable_cursor_planning_mode = true,
+    enable_claude_text_editor_tool_mode = true,
+		auto_apply_diff_after_generation = false,
+		support_paste_from_clipboard = false,
+		minimize_diff = true, -- Whether to remove unchanged lines when applying a code block
+	},
+	hints = {
+		enabled = false,
+	},
+	windows = {
+		sidebar_header = {
+			rounded = false,
+		},
+		edit = {
+			border = "none",
+		},
+		ask = {
+			border = "none",
+		},
+	},
+	mappings = {
+		diff = {
+			ours = "co",
+			theirs = "ct",
+			all_theirs = "ca",
+			both = "cb",
+			cursor = "cc",
+			next = "]x",
+			prev = "[x",
+		},
+		suggestion = {
+			accept = "<M-l>",
+			next = "<M-]>",
+			prev = "<M-[>",
+			dismiss = "<C-]>",
+		},
+		jump = {
+			next = "]]",
+			prev = "[[",
+		},
+		submit = {
+			normal = "<CR>",
+			insert = "<C-s>",
+		},
+		sidebar = {
+			apply_all = "A",
+			apply_cursor = "a",
+			switch_windows = "<Tab>",
+			reverse_switch_windows = "<S-Tab>",
+		},
+	},
 })
