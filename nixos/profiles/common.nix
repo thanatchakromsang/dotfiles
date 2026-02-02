92b8dfe0ec5d249ebb3bd13844cfa1adde78f776
Thanatchaya Kromsaeng <thanatchakromsang@gmail.com>
2026-02-02 10:32:30 +0700

    refactor(profiles): move user-level config to home-manager
    - Move packages, shell aliases, and zsh config from common.nix to user home-manager
    - Add fzf-tab plugin and terraform alias to user zsh config
    - Set SOPS_AGE_KEY_FILE in user session variables
    - Simplify common.nix to only manage imports and system-level zsh

--- nixos/profiles/common.nix
+++ nixos/profiles/common.nix
@@ -3,96 +3,17 @@
 
 {
   imports =
-    [
-      ../services/ssh.nix
-      ../services/ntp.nix
-      ../services/localization.nix
+  [
+    ../services/ssh.nix
+    ../services/ntp.nix
+    ../services/localization.nix
 
-      ./workspace/lazygit.nix
+    ./workspace/lazygit.nix
+    ./workspace/tmux.nix
 
-      ./applications/neovim.nix
-      ./applications/opencode/opencode.nix
-    ];
+    ./applications/neovim.nix
+    ./applications/opencode/opencode.nix
+  ];
 
-  environment.systemPackages = with pkgs;
-    [
-      git
-      rsync
-      htop
-      wget
-      curl
-      inetutils
-      unzip
-      p7zip
-      jq
-      yq-go
-      vim
-      eza
-      bat
-      stow
-      fzf
-      fd
-      gnupg
-      ripgrep
-      ranger
-      bitwarden-cli
-      bitwarden-desktop
-      zsh-powerlevel10k
-
-      lsof
-      age
-      sops
-      ssh-to-age
-    ];
-
-  programs.zsh = {
-    enable = true;
-    enableCompletion = true;
-    autosuggestions.enable = true;
-    syntaxHighlighting.enable = true;
-    histSize = 1000000000;
-    ohMyZsh = {
-      enable = true;
-      plugins =
-        [
-          "git"
-          "yarn"
-          "docker"
-          "node"
-          "npm"
-          "kubectl"
-          "wd"
-          "fzf"
-          "helm"
-          "rust"
-          "aws"
-          "history-substring-search"
-        ];
-    };
-  };
-
-  environment.variables = {
-    "EDITOR" = "nvim";
-    "VISUAL" = "nvim";
-    "FZF_BASE" = "${pkgs.fzf}/share/fzf";
-    "FZF_DEFAULT_OPTS" = "--height 40% --border --reverse";
-    "FZF_DEFAULT_COMMAND" = "fd --type f --hidden --follow --exclude .git";
-  };
-
-  environment.shellAliases = {
-    cat = "${pkgs.bat}/bin/bat";
-    grep = "${pkgs.ripgrep}/bin/rg";
-    ls = "${pkgs.eza}/bin/eza";
-    l = "ls -ln"; # size,show type,human readable
-    la = "ls -lAn"; # long list,show almost all,show type,human readable
-    lt = "ls -lth"; # long list,sorted by date,show type,human readable
-    ll = "ls -l"; # long list
-    ldot = "ls -ld .*";
-    k = "${pkgs.kubectl}/bin/kubectl";
-    tmp = "vim ~/.tmp.md";
-    g = "${pkgs.lazygit}/bin/lazygit";
-    r = "ranger --choosedir=$HOME/.rangerdir; LASTDIR=`cat $HOME/.rangerdir`; cd $LASTDIR";
-    c = "clear";
-    ssh = "TERM=xterm-256color ssh";
-  };
+  programs.zsh.enable = true;
 }
--- nixos/users/thanatchaya/base.nix
+++ nixos/users/thanatchaya/base.nix
@@ -14,8 +14,11 @@
   };
 
   home-manager.users.thanatchaya = { pkgs, config, ... }: {
-    # Workaround for firefox pkgs error ref: https://github.com/nix-community/home-manager/issues/2010
     home.stateVersion = "25.11";
+
+    nixpkgs.config.allowUnfree = true;
+
+    # config.allowUnfree = true;
 
     programs.zsh = {
       enable = true;
@@ -51,6 +54,46 @@
 
         [[ ! -f ~/.p10k.zsh ]] || source ~/.p10k.zsh
       '';
+      enableCompletion = true;
+      autosuggestion.enable = true;
+      syntaxHighlighting.enable = true;
+
+      shellAliases = {
+        cat = "${pkgs.bat}/bin/bat";
+        grep = "${pkgs.ripgrep}/bin/rg";
+        ls = "${pkgs.eza}/bin/eza";
+        l = "ls -ln";
+        la = "ls -lAn";
+        lt = "ls -lth";
+        ll = "ls -l";
+        ldot = "ls -ld .*";
+        k = "${pkgs.kubectl}/bin/kubectl";
+        tmp = "vim ~/.tmp.md";
+        g = "${pkgs.lazygit}/bin/lazygit";
+        r = "ranger --choosedir=$HOME/.rangerdir; LASTDIR=`cat $HOME/.rangerdir`; cd $LASTDIR";
+        c = "clear";
+        ssh = "TERM=xterm-256color ssh";
+        tf = "${pkgs.terraform}/bin/terraform";
+      };
+
+      "oh-my-zsh" = {
+        enable = true;
+        plugins = [
+          "git"
+          "docker"
+          "yarn"
+          "node"
+          "npm"
+          "kubectl"
+          "wd"
+          "fzf"
+          "helm"
+          "rust"
+          "aws"
+          "history-substring-search"
+        ];
+      };
+
       plugins = [
         {
           name = "zsh-nix-shell";
@@ -60,6 +103,16 @@
             repo = "zsh-nix-shell";
             rev = "v0.4.0";
             sha256 = "037wz9fqmx0ngcwl9az55fgkipb745rymznxnssr3rx9irb6apzg";
+          };
+        }
+        {
+          name = "fzf-tab";
+          file = "fzf-tab.plugin.zsh";
+          src = pkgs.fetchFromGitHub {
+            owner = "Aloxaf";
+            repo = "fzf-tab";
+            rev = "c7fb028ec0bbc1056c51508602dbd61b0f475ac3";
+            sha256 = "061jjpgghn8d5q2m2cd2qdjwbz38qrcarldj16xvxbid4c137zs2";
           };
         }
       ];
@@ -73,6 +126,45 @@
       };
     };
 
+    home.packages = with pkgs; [
+      git
+      rsync
+      htop
+      wget
+      curl
+      inetutils
+      unzip
+      p7zip
+      jq
+      yq-go
+      vim
+      eza
+      bat
+      stow
+      fzf
+      fd
+      gnupg
+      ripgrep
+      ranger
+      bitwarden-cli
+      bitwarden-desktop
+      zsh-powerlevel10k
+      kubectl
+
+      age
+      sops
+      ssh-to-age
+    ];
+
+    home.sessionVariables = {
+      EDITOR = "nvim";
+      VISUAL = "nvim";
+      FZF_BASE = "${pkgs.fzf}/share/fzf";
+      FZF_DEFAULT_OPTS = "--height 40% --border --reverse";
+      FZF_DEFAULT_COMMAND = "fd --type f --hidden --follow --exclude .git";
+      SOPS_AGE_KEY_FILE = "${config.home.homeDirectory}/.ssh/personal.pri.age.txt";
+    };
+
     # ~/.config symlinks
     xdg.configFile = {
       "ranger" = { source = config.lib.file.mkOutOfStoreSymlink (config.home.homeDirectory + "/.dotfiles/configs/ranger"); recursive = true; };
@@ -83,15 +175,6 @@
       ".editorconfig".source = config.lib.file.mkOutOfStoreSymlink (config.home.homeDirectory + "/.dotfiles/home/editorconfig");
       ".p10k.zsh".source = config.lib.file.mkOutOfStoreSymlink (config.home.homeDirectory + "/.dotfiles/home/p10k.zsh");
     };
-
-    /* # TODO: XDG MIMEAPPS without conflict */
-    /* xdg.mimeApps = { */
-    /*   enable = true; */
-    /*   defaultApplications = { */
-    /*     "application/pdf" = [ "zathura" "firefox.desktop" ]; */
-    /*     "video/H264" = [ "mpv.desktop" ]; */
-    /*   }; */
-    /* }; */
 
     # disable keyboard management using home-manager
     home.keyboard = null;
