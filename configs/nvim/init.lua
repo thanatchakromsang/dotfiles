bce0b01d6352480af330ef9be2a5e26264eda68c
Thanatchaya Kromsaeng <thanatchakromsang@gmail.com>
2026-04-06 16:48:29 +0700

    fix: restore nvim init.lua for non-home-manager machines
    init.lua is now managed by home-manager on NixOS machines via
    programs.neovim.initLua, but kept here as a regular file so the
    config works when dotfiles are used without home-manager.

---
+++ configs/nvim/init.lua
@@ -0,0 +1,18 @@
+_G.__luacache_config = {
+  chunks = {
+    enable = true,
+    path = vim.fn.stdpath('cache')..'/luacache_chunks',
+  },
+  modpaths = {
+    enable = true,
+    path = vim.fn.stdpath('cache')..'/luacache_modpaths',
+  }
+}
+vim.loader.enable()
+require('options')
+require('clipboards')
+require('keymappings')
+require('utils')
+require('lsp')
+require('theme')
+require('plugins')
